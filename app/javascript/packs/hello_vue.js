bff9f255846de92cca26593227cefe15f1c09c74
Charlie Cooksey <cdcooksey@gmail.com>
2018-09-29 21:41:33 -0600

    updates Vue landing page setup

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -3,29 +3,24 @@
 // <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
 // to the head of your layout file,
 // like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.  
+import Vue from 'vue'
+import App from '../app.vue'
+// import Vue from 'vue/dist/vue.esm'
 
-// import Vue from 'vue'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const el = document.body.appendChild(document.createElement('hello'))
-//   const app = new Vue({
-//     el,
-//     render: h => h(App)
-//   })
-//
-//   console.log(app)
-// })
-import Vue from 'vue/dist/vue.esm'
 document.addEventListener('DOMContentLoaded', () => {
-    var app = new Vue({
-          el: '#app',
-          data: {
-                  message: 'Hello Vue!'
-                }
-        })
+  const el = document.body.appendChild(document.createElement('hello'))
+  const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'what up!'
+    },
+    render: h => h(App)
+  })
+
+  console.log(app)
 })
+
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
 // need to always use single file components.
